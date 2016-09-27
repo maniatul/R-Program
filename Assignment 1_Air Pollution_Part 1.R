66a5c85b96c0433e5b340c8acf3da64eb45269ec
maniatul <maniatuliet@gmail.com>
2016-09-27 14:10:17 +0530

    Update Assignment 1_Air Pollution_Part 1.R

--- Assignment 1_Air Pollution_Part 1.R
+++ Assignment 1_Air Pollution_Part 1.R
@@ -1,26 +1,27 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   
+  #store the current working directorr in a vriable
   old.dir <- getwd()
-  setwd("~/Documents/DS/specdata")
   
+  #go to the directory having data files
+  setwd("~/Documents/DS/specdata")
   
   #store file names in a variable
   fl_names <- list.files(path = ".", pattern = ".csv")
   
-  #creat data frame of given files
+  #creat a data frame for required files
   dt_frm <- data.frame()
-  for(i in 1:length(fl_names[id])) {
-  dt_frm <- rbind(dt_frm, read.csv(fl_names[id[i]]))
-  }
-  head(dt_frm)
+  
+  #store the required data in data frame as per the id
+  for(i in 1:length(fl_names[id])) 
+    {
+      dt_frm <- rbind(dt_frm, read.csv(fl_names[id[i]]))
+    }
+  
+  # Print the output mean
   print(mean(dt_frm[, pollutant], na.rm = 1))
   
+  #return to the original working directory
   setwd(old.dir)
   
-
-  
-
 }
-
-
-
